1ca056757595183e2faced47c74c9481daaa8056
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-17 17:57:22 +0900

    ログイン、ログアウト機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except:[:index, :show]
+
   def index
-   
+
+  end
+
+  def new
+    
+  end
+
+  def show
+    
   end
 
   
