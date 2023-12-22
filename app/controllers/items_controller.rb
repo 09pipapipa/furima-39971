fc03fcbf425238daf3ad31248dbab795958842be
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-22 11:14:45 +0900

    item view実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,23 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
 
   def index
+    @items = Item.all
+  end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @Item = Item.new(item_params)
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
   end
 
   def new
@@ -18,6 +34,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:image).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:goods_name, :price, :exposition, :category_id, :condition_id, :cost_id, :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
   end
 end
