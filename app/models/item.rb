e086d6b3ef3a7a48eb7cc6bdaa1b6f387b8ca08e
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-22 10:27:35 +0900

    バリデーション記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
 #has_one   :buy
 has_one_attached :image
 belongs_to :user
+
+validates :goods_name,presence: true
+validates :price,presence: true,numericality: { greater_than: 0}
+validates :exposition,presence: true
+validates :category_id,presence: true
+validates :condition_id,presence: true
+validates :cost_id,presence: true
+validates :prefecture_id,presence: true
+validates :delivery_date_id,presence: true
 end
