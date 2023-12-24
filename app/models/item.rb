c17637266b67af3e5f37312370698fe579b38df0
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-24 11:49:54 +0900

    価格下限、上限設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   validates :goods_name,presence: true
-  validates :price,presence: true,numericality: { greater_than: 0}
+  validates :price,presence: true, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :exposition,presence: true
   validates :category_id,presence: true
   validates :condition_id,presence: true
