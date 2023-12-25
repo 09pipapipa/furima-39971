31e5a3951af7378a55e28467b1befc9abfb8a878
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-25 16:08:44 +0900

    itemテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,77 +17,77 @@ RSpec.describe Item, type: :model do
       it 'goods_nameが空では登録できない' do
         @item.goods_name = nil
         @item.valid?
-        expect(@item.errors[:goods_name]).to include("can't be blank")
+        expect(@item.errors.full_messages).to include("Goods name can't be blank")
       end
       it 'expositionが空では登録できない' do
         @item.exposition = nil
         @item.valid?
-        expect(@item.errors[:exposition]).to include("can't be blank")
+        expect(@item.errors.full_messages).to include("Exposition can't be blank")
       end
       it 'imageが空では登録できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors[:image]).to include("can't be blank")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'categoryは1以外でないと登録できない' do
-        @item.category_id = nil
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors[:category_id]).to include('must be other than 1')
+        expect(@item.errors.full_messages).not_to include("Category must be other than 1")
       end
       it 'conditionは1以外でないと登録できない' do
-        @item.condition_id = nil
+        @item.condition_id = 1
         @item.valid?
-        expect(@item.errors[:condition_id]).to include('must be other than 1')
+        expect(@item.errors.full_messages).not_to include("Condition must be other than 1")
       end
       it 'costは1以外でないと登録できない' do
-        @item.cost_id = nil
+        @item.cost_id = 1
         @item.valid?
-        expect(@item.errors[:cost_id]).to include('must be other than 1')
+        expect(@item.errors.full_messages).not_to include("Cost must be other than 1")
       end
       it 'prefectureは1以外でないと登録できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors[:prefecture_id]).to include('must be other than 1')
+        expect(@item.errors.full_messages).not_to include("Prefecture must be other than 1")
       end
       it 'delivery_dateは1以外でないと登録できない' do
         @item.delivery_date_id = 1
         @item.valid?
-        expect(@item.errors[:delivery_date_id]).to include('must be other than 1')
+        expect(@item.errors.full_messages).not_to include("Delivery Date must be other than 1")
       end
       it 'priceは300未満では登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors[:price]).to include('must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceは10000000以上は登録できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors[:price]).to include('must be less than or equal to 9999999')
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'priceは全角英字では登録できない' do
         @item.price = 'ｗｔｒ'
         @item.valid?
-        expect(@item.errors[:price]).to include('is not a number')
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'priceは半角文字では登録できない' do
         @item.price = 'abc'
         @item.valid?
-        expect(@item.errors[:price]).to include('is not a number')
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'priceは全角数字では登録できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors[:price]).to include('is not a number')
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'priceは半角英数字混合では登録できない'do
       @item.price = 'abc123'
       @item.valid?
-      expect(@item.errors[:price]).to include('is not a number')
+      expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'user_idが紐づいていないと登録できない' do
         @item.user_id = nil
         @item.valid?
-        expect(@item.errors[:user_id]).to include('must exist')
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
