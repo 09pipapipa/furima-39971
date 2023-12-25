db478fca7bdba04d7b938eb1fbaa1326fa79e198
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-25 19:53:50 +0900

    rubocop実行２回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,15 @@ class Item < ApplicationRecord
   belongs_to :cost_id
   belongs_to :prefecture_id
   belongs_to :delivery_date_id
-  
+
   validates :goods_name, presence: true
   validates :price, presence: true,
                     numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :exposition,      presence: true
-  validates :category_id,     presence: true, numericality: {other_than: 1, message: "can't be blank"}
-  validates :condition_id,    presence: true, numericality: {other_than: 1, message: "can't be blank"}
-  validates :cost_id,         presence: true, numericality: {other_than: 1, message: "can't be blank"}
-  validates :prefecture_id,   presence: true, numericality: {other_than: 1, message: "can't be blank"}
-  validates :delivery_date_id,presence: true, numericality: {other_than: 1, message: "can't be blank"}
-  validates :image,           presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :cost_id,         presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
 end
