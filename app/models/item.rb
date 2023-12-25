748669ff13c8a0c6c8b8914706da5e07e1ded386
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-25 20:52:21 +0900

    itemバリデーション再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,17 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Association::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # has_one   :buy
   has_one_attached :image
   belongs_to :user
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :cost
-  belongs_to :prefecture
-  belongs_to :delivery_date
+  belongs_to :category,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  belongs_to :condition,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  belongs_to :cost,            presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  belongs_to :prefecture,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  belongs_to :delivery_date,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
 
   validates :goods_name, presence: true
   validates :price, presence: true,
                     numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :exposition,      presence: true
-  validates :category,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :cost,            presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_date,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :image, presence: true
 end
