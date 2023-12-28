c3ef8ede1d2cce6b954171e397112a23d3500350
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-28 14:42:16 +0900

    編集画面表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index,except: [:index]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -25,6 +26,11 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user
+      redirect_to root_path unless @item.user == current_user
+    else
+      redirect_to login_path
+    end
   end
 
   private
@@ -32,5 +38,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:goods_name, :price, :exposition, :category_id, :condition_id, :cost_id, :prefecture_id,
                                  :delivery_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
