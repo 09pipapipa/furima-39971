c5c5569894406f215f58a1d6a6b4fa8eb0ea3e35
09pipapipa <pipapipa.hold.up@gmail.com>
2023-12-29 16:51:21 +0900

    controllerコードまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index]
+  before_action :set_item,only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,11 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user
       redirect_to root_path unless @item.user.id == current_user.id
     else
@@ -34,7 +33,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -43,6 +41,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:goods_name, :price, :exposition, :category_id, :condition_id, :cost_id, :prefecture_id,
