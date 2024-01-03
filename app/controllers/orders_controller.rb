ebb7e71a67e865934f406debf198e5618374bc6e
09pipapipa <pipapipa.hold.up@gmail.com>
2024-01-03 15:21:00 +0900

    フォーム⇒保存可能に記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @orders = Order.all
+    @items = Item.all
   end
 
   def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:item).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(order_id: @order.id)
   end
 
 end
