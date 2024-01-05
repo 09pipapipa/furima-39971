bd04ef3a7a3e60f49757bc5a74ecb0c5d8b715b1
09pipapipa <pipapipa.hold.up@gmail.com>
2024-01-05 18:02:04 +0900

    validates修正、city追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,6 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Enter only half-width numerals with 10 to 11 digits" }
     validates :item_id
@@ -14,9 +13,10 @@ class OrderAddress
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
+
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code,prefecture_id: prefecture_id, street_address: street_address,
-                   building_name: building_name, phone_number: phone_number,order_id: order.id)
+                   city: city,building_name: building_name, phone_number: phone_number,order_id: order.id)
   end
 end
