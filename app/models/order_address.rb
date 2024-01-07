d50b85e06a22907a045bd05dcfb262165d88b591
09pipapipa <pipapipa.hold.up@gmail.com>
2024-01-07 18:10:12 +0900

    アソシエーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,15 +4,16 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number,:item_id, :user_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id,    numericality: {other_than: 1, message: "can't be blank"}
+    validates :postal_code,      format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :phone_number,     format: { with: /\A\d{10,11}\z/, message: "Enter only half-width numerals with 10 to 11 digits" }
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Enter only half-width numerals with 10 to 11 digits" }
     validates :item_id
     validates :user_id
     validates :city
     validates :token
   end
-  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  
   
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
