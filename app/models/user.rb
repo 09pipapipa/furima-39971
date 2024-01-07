7545847799856b3c859ea791c1c2f4f38e532ea0
09pipapipa <pipapipa.hold.up@gmail.com>
2024-01-07 17:34:52 +0900

    usermodel password削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_one  :order
 
   validates :nickname,              presence: true
-  validates :password,              presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :birthday,              presence: true
   validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー－]+\z/ }
   validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー－]+\z/ }
