f61a10f5a384a0ba11853e0f11ef0e69a8ea3707
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-01-20 12:57:21 +0900

    文言微調整

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,8 +5,8 @@ class OrderAddress
 
   with_options presence: true do
     validates :prefecture_id,    numericality: {other_than: 1, message: "を入力してください"}
-    validates :postal_code,      format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンを(-)入力してください" }
-    validates :phone_number,     format: { with: /\A\d{10,11}\z/, message: "10桁から11桁の半角数字のみを入力してください" }
+    validates :postal_code,      format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを(-)入力してください" }
+    validates :phone_number,     format: { with: /\A\d{10,11}\z/, message: "は10桁から11桁の半角数字のみを入力してください" }
     validates :street_address
     validates :item_id
     validates :user_id
