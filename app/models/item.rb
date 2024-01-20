1eb150b3c947cd3e52e28eb5f8b0f408bfcfd7f2
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-01-20 12:50:37 +0900

    エラーメッセージ日本語化model変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
                     numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :exposition,    presence: true
   validates :image,         presence: true
-  validates :category_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id,      presence: true, numericality: { other_than: 1, message: "を入力してください" }
   validates :condition_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :cost_id,          presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
