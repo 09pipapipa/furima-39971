fd83f994912d7dc75566d850cff63836342ad230
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-01-21 10:05:36 +0900

    model修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_date
 
-  validates :goods_name, presence: true
+  validates :goods_name, presence: true,
   validates :price, presence: true,
-                    numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :exposition,    presence: true
-  validates :image,         presence: true
+                    numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  validates :exposition,    presence: true,
+  validates :image,         presence: true,
   validates :category_id,      presence: true, numericality: { other_than: 1, message: "を入力してください" }
   validates :condition_id,     presence: true, numericality: { other_than: 1, message: "を入力してください" }
   validates :cost_id,          presence: true, numericality: { other_than: 1, message: "を入力してください" }
